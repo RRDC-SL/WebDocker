ed3c58343f83a7b2408025de907285359e74dde9
RaithSphere Starpaw <raithsphere@gmail.com>
2020-04-16 14:58:30 +0100

    Update entry.sh
    Correct that...

--- entry.sh
+++ entry.sh
@@ -3,7 +3,7 @@
 mkdir -p /usr/share/webapps/ && cd /usr/share/webapps/ && \
     wget https://files.phpmyadmin.net/phpMyAdmin/5.0.2/phpMyAdmin-5.0.2-all-languages.tar.gz > /dev/null 2>&1 && \
     tar -xzvf phpMyAdmin-5.0.2-all-languages.tar.gz > /dev/null 2>&1 && \
-    mv phpMyAdmin-5.0.2-all-languages.tar.gz phpmyadmin && \
+    mv phpMyAdmin-5.0.2-all-languages phpmyadmin && \
     chmod -R 777 /usr/share/webapps/ && \
     ln -s /usr/share/webapps/phpmyadmin/ /var/www/localhost/htdocs/phpmyadmin
     
